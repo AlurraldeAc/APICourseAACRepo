633a62c68662230b09f78ff5dcd5a0f15b7e71ff
AlurraldeAc <62907692+AlurraldeAc@users.noreply.github.com>
2024-03-31 21:14:50 -0400

    RestClient update - Validation purpose
    This is to store these three elements from the the response in a dictionary
            response["status_code"]
            response["headers"]
            response["body"]
    - example of use in tests:
    assert response["status_code"] == 201

--- gorest_api/users/test_users.py
+++ gorest_api/users/test_users.py
@@ -16,28 +16,50 @@ class TestUsers:
         cls.rest_client = RestClient()
         cls.url_gorest_users = f"{URL_GOREST}/users"
         response = cls.rest_client.request("get", cls.url_gorest_users)
+<<<<<<< Updated upstream
         cls.user_id = response.json()[0]["id"]
         LOGGER.debug("User Id: %s", cls.user_id)
         cls.user_created_list = []
 
     def test_get_all_users(self):
+=======
+        cls.user_id = response["body"][0]["id"]
+        LOGGER.debug("User Id: %s", cls.user_id)
+        cls.user_created_list = []
+
+    def test_get_all_users(self, log_test_names):
+>>>>>>> Stashed changes
         """
         Test get all user from GET endpoint
         """
         LOGGER.info("Test GET all users")
         response = self.rest_client.request("get", self.url_gorest_users)
+<<<<<<< Updated upstream
         assert response.status_code == 200
 
     def test_get_user(self):
+=======
+        # From assert response.status_code == 200  --> assert response["status_code"]== 200
+        # To
+        assert response["status_code"] == 200
+
+    def test_get_user(self, log_test_names):
+>>>>>>> Stashed changes
         """
         Test get a specific user from GET endpoint
         """
         LOGGER.info("Test GET a user by Id")
         url_get_user = f"{self.url_gorest_users}/{self.user_id}"
         response = self.rest_client.request("get", url_get_user)
+<<<<<<< Updated upstream
         assert response.status_code == 200
 
     def test_create_user(self):
+=======
+        assert response["status_code"] == 200
+
+    def test_create_user(self, log_test_names):
+>>>>>>> Stashed changes
         """
         Test create a new user (post method)
         """
@@ -49,31 +71,55 @@ class TestUsers:
             "status": "active"
         }
         response = self.rest_client.request("post", self.url_gorest_users, body=body_request)
+<<<<<<< Updated upstream
         if response.status_code == 201:
             self.user_created_list.append(response.json()["id"])
 
         assert response.status_code == 201
 
     def test_update_user(self):
+=======
+        if response["status_code"] == 201:
+            self.user_created_list.append(response["body"]["id"])
+
+        assert response["status_code"] == 201
+
+    def test_update_user(self, create_user, log_test_names):
+>>>>>>> Stashed changes
         """
         Test update user (the last created)
         """
         LOGGER.info("Test update user")
+<<<<<<< Updated upstream
         url_update_user = f"{self.url_gorest_users}/{self.user_id}"
         body_request_update = {
             "name": "Rolando Baldez",
             "email": "bladezrol@mail.com",
+=======
+        url_update_user = f"{self.url_gorest_users}/{create_user}"
+        body_request_update = {
+            "name": "Marco Fio",
+            "email": "markCleo@mail.com",
+>>>>>>> Stashed changes
             "gender": "male",
             "status": "inactive"
         }
         LOGGER.info("User to update, %s", self.user_id)
         response = self.rest_client.request("put", url_update_user, body=body_request_update)
+<<<<<<< Updated upstream
         if response.status_code == 200:
             self.user_created_list.append(response.json()["id"])
 
         assert response.status_code == 200
 
     def test_delete_user(self, create_user):
+=======
+        if response["status_code"] == 200:
+            self.user_created_list.append(response["body"]["id"])
+        assert response["status_code"] == 200
+
+    def test_delete_user(self, create_user, log_test_names):
+>>>>>>> Stashed changes
         """
         Test delete a user
         """
@@ -81,8 +127,15 @@ class TestUsers:
         url_delete_user = f"{self.url_gorest_users}/{create_user}"
         LOGGER.info("user Id to be deleted : %s", create_user)
         response = self.rest_client.request("delete", url_delete_user)
+<<<<<<< Updated upstream
         assert response.status_code == 204
 
+=======
+        assert response["status_code"] == 204
+
+# This teardown class method would be unnecessary if the "test_create_user" test
+# were not leaving single user alone that it not being deleted after all tests.
+>>>>>>> Stashed changes
     @classmethod
     def teardown_class(cls):
         """
@@ -93,5 +146,9 @@ class TestUsers:
         for user_id in cls.user_created_list:
             url_delete_user = f"{cls.url_gorest_users}/{user_id}"
             response = cls.rest_client.request("delete", url_delete_user)
+<<<<<<< Updated upstream
             if response.status_code == 204:
+=======
+            if response["status_code"] == 204:
+>>>>>>> Stashed changes
                 LOGGER.info("project Id deleted : %s", user_id)
